36aee5352f05d8dfe06aa6f100732538b9bd2718
Milan <milangru@gmail.com>
2024-01-18 22:56:27 +0100

    Fixed the solution to actually calculate leap years

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let c = 0
 for (let i = 2000; i<3001; i++) {
-    if (i % 400 === 0) {
+    if (i % 4 === 0 && i % 100 !== 0 || i % 400 === 0) {
         console.log(i)
+        c += 1
     }
 }
+console.log(c)
